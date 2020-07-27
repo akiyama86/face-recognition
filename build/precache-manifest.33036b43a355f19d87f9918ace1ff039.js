240ca7e62747255528b48fe74fd60d5568e2e1bb
akiyama86 <akirayamashita86@gmail.com>
2020-07-27 17:35:28 +0900

    add build

---
+++ build/precache-manifest.33036b43a355f19d87f9918ace1ff039.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846799cd5313e8b925d5191533cff167",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4195a6bd536fd8fcd9fb",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b1e8df99b9864ae0dd22",
+    "url": "/static/css/main.76a14b35.chunk.css"
+  },
+  {
+    "revision": "4195a6bd536fd8fcd9fb",
+    "url": "/static/js/2.42399715.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42399715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e8df99b9864ae0dd22",
+    "url": "/static/js/main.19feb9f7.chunk.js"
+  },
+  {
+    "revision": "c9b0a537ac9221472b6e",
+    "url": "/static/js/runtime-main.30657a9b.js"
+  }
+]);
